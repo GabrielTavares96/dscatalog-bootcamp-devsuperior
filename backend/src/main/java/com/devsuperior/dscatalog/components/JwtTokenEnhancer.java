5adfbe16e47400c9f0c1b961a0c05e211471ba58
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-05-06 11:22:11 -0300

    Token additional information

---
+++ backend/src/main/java/com/devsuperior/dscatalog/components/JwtTokenEnhancer.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dscatalog.components;
+
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken oAuth2AccessToken, OAuth2Authentication oAuth2Authentication) {
+
+        User user = userRepository.findByEmail(oAuth2Authentication.getName());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userFIrstName", user.getFirstName());
+        map.put("usrId", user.getId());
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) oAuth2AccessToken;
+        token.setAdditionalInformation(map);
+
+        return oAuth2AccessToken;
+    }
+}
